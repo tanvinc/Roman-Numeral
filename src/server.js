5941026d98da56b251ae28edcd910c8a85d5c1ed
Tanvi Nachane <tanvi.nachane@outlook.com>
2021-02-03 00:06:13 -0700

    initial server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,10 @@
-// server code
+'use strict';
+
+const express=require('express');
+const app=express();
+const port=8080;
+const log=require('./logger');
+
+app.listen(port, () => {
+    log.info('Server is running on port %d', port);
+});
