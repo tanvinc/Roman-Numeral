4cbc81c458e3e6a33fae4134ce4791a8ba5cdfb2
Tanvi Nachane <tanvi.nachane@outlook.com>
2021-02-03 00:22:45 -0700

    handling get requests

--- src/server.js
+++ src/server.js
@@ -5,6 +5,23 @@ const app=express();
 const port=8080;
 const log=require('./logger');
 
+/**
+ * Handling GET requests on the base path
+ */
+app.get('/', async function(req, res) {
+
+    log.info('Request received');
+
+    res.send({
+        status: 200,
+        message: 'Hello'
+    });
+});
+
+
+/**
+ * Configuring the service to listen on localhost port 8080
+ */
 app.listen(port, () => {
     log.info('Server is running on port %d', port);
 });
