eb31f0a93cd70497b0e3f632574ba66e0fbf00fa
Tanvi Nachane <tanvi.nachane@outlook.com>
2021-02-07 02:02:51 -0700

    moving test and lint config

---
+++ src/test/test.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-undef */
+'use strict';
+
+const app=require('../convert');
+const chai=require('chai');
+
+// eslint-disable-next-line no-unused-vars
+const should=chai.should();
+
+// eslint-disable-next-line no-undef
+describe('testing the function for generating roman numerals', () =>{
+    let ans;
+    it('should return a valid roman numeral', async () => {
+        ans=await app(357);
+        ans.should.equal('CCCLVII');
+    });
+
+    it('should return an incorrect roman numeral', async () => {
+        ans=await app(9000);
+        ans.should.equal('MMMMMMMMM');
+    });
+
+    it('should return an empty string for a negative number', async () => {
+        ans=await app(-8000);
+        ans.should.equal('');
+    });
+
+    it('should return an error for MAX_VALUE', async () => {
+        const x = Number.MAX_VALUE;
+        let err=new Error('Unable to process a Roman Numeral for the input');
+        try{
+            ans=await app(x);
+        } catch(e){
+            e.should.equal(err);
+        }  
+    });
+
+    it('should return an error for MIN_VALUE', async () => {
+        const y = Number.MIN_VALUE;
+        let err=new Error('Unable to process a Roman Numeral for the input');
+        try{
+            ans=await app(y);
+        } catch(e){
+            e.should.equal(err);
+        }  
+    }); 
+});
