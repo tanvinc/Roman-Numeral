1cf57e9e6c22fbf56cdc5ba516a92241b822efaa
Tanvi Nachane <tanvi.nachane@outlook.com>
2021-02-07 03:12:36 -0700

    adding metrics

--- src/server.js
+++ src/server.js
@@ -2,18 +2,29 @@
 
 const express=require('express');
 const app=express();
-const port=8080;
+const port=process.env.PORT;
 const log=require('./logger');
 const converter=require('./convert');
+const metrics = require('datadog-metrics'); // the service uses a datadog agent configured using a datadog API key to push metrics to Datadog
+let env= process.env.ENV;
+metrics.init({ host: '127.0.0.1', prefix: 'roman-numeral.' }); // initializing metrics for localhost, prefixed with name of the service
+
+
 
 /**
- * Handling GET requests for roman numerals
+ * Handling GET requests for roman numerals and generating metrics
+ * Metrics being pushed to datadog:
+ *  - Number of requests received
+ *  - Number of successfully processed requests
+ *  - Number of failed requests
+ *  - Types of response codes (this is to fetch counts per response code)
  */
 app.get('/romannumeral', async function (req, res) {
 
     try{
 
         log.info('Request received');
+        metrics.increment('requests_received',  1, [`env:${env}`]);
         
         if(Object.keys(req.query).length==1){ // checking if there are more than one query parameters
 
@@ -28,17 +39,29 @@ app.get('/romannumeral', async function (req, res) {
                     output: output
                 });
 
+                metrics.increment('responsecodes', 1, ['status_code:200',  `env:${env}`]); // this metric keeps track of response codes
+                metrics.increment('successful_requests', 1, [`env:${env}`]); // this metric keeps track of successfully process requests
+
             } else{
                 res.status(422).send({error: 'Input out of bounds or cannot be processed'}); // fails to process negative integers, integers greater than 3999 or less than 1 and alphabetic or special character inputs
                 log.error('The input %d is out of bounds', num);
+                
+                metrics.increment('responsecodes', 1, ['status_code:422',  `env:${env}`]);
+                metrics.increment('failed_requests', 1, [`env:${env}`]); // this metric keeps track of failed requests
             }
         } else{
             res.status(422).send({error: 'Request parameters cannot be processed'}); // failed to process requests with multiple query parameters
             log.error('More than one parameter found');
+            
+            metrics.increment('responsecodes', 1, ['status_code:422',  `env:${env}`]);
+            metrics.increment('failed_requests', 1, [`env:${env}`]);
         }
     } catch(e) { // block to catch unexpected errors in processing
         res.status(500).send({error: 'Unexpected error'});
         log.error('Unexpected error %d', e);
+        
+        metrics.increment('responsecodes', 1, ['status_code:500',  `env:${env}`]);
+        metrics.increment('failed_requests', 1, [`env:${env}`]);
     }
 });
 
